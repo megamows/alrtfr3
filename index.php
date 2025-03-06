f5849dd642b7383508f18e8de065ff1d6c97119b
MEGG <megamows.media@gmail.com>
2025-03-06 21:37:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -53,11 +53,11 @@
         //var phone_number = '919 610 587';
         var phone_number = '+33 9 85 40 99 89';
 </script>
-<!-- Hotjar Tracking Code for checksys.netlify.app -->
+<!-- Hotjar Tracking Code for checksys fr -->
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5326173,hjsv:6};
+        h._hjSettings={hjid:5329113,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
